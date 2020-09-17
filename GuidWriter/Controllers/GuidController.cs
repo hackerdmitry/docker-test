6e0daa9756ba21bae9848718cbc97bfd15125855
hackerdmitry <hacker.dmitry@gmail.com>
2020-09-17 23:57:53 +0500

    init

---
+++ GuidWriter/Controllers/GuidController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuidWriter.Controllers
+{
+    [ApiController]
+    [Route("")]
+    [Route("Guid")]
+    public class GuidController : ControllerBase
+    {
+        [Route("")]
+        [Route("Write")]
+        public string Write()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
